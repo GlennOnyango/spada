cd74f4b88432cb424db9d61b574a8b851624691c
GlennOnyango <glennonyango@yahoo.com>
2024-02-26 16:41:06 +0300

    Forms work is done

--- index.js
+++ index.js
@@ -37,3 +37,101 @@ const tooltipList = [...tooltipTriggerList].map(
 //     }, (10 * i) / 10);
 //   }
 // }
+
+document.querySelector(".quote_form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const first_name = document.querySelector("#first_name").value;
+  const last_name = document.querySelector("#last_name").value;
+  const email = document.querySelector("#email").value;
+  const phone = document.querySelector("#phone_number").value;
+  const service = document.querySelector("#service").value;
+  const message = document.querySelector("#message").value;
+
+  console.log(first_name, last_name, email, phone, service,message);
+
+  fetch("https://sapdalimited.co.ke/php/", {
+    method: "POST",
+    body: JSON.stringify({
+      data: {
+        first_name,
+        last_name,
+        email,
+        phone,
+        service,
+        message,
+        "qoute-form": "true",
+      },
+    }),
+  })
+    .then((res) => {
+      console.log(res);
+
+      if (!res.ok) {
+        throw new Error("An error occured");
+      } else {
+        document.querySelector(".quote_form").reset();
+        document.querySelector(".response-sector").innerHTML = `
+        <div class="alert alert-success" role="alert">
+          Dear ${first_name} ${last_name},
+          Thank you for reaching out, a member of our organisation will get back to you shortly.
+        </div>
+        `;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      document.querySelector(".response-sector").innerHTML = `
+      <div class="alert alert-danger" role="alert">
+        An error occured, please try again later.
+      </div>
+      `;
+    });
+});
+
+document.querySelector(".contact_form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const first_name = document.querySelector("#contact_first_name").value;
+  const last_name = document.querySelector("#contact_last_name").value;
+  const email = document.querySelector("#contact_email").value;
+  const phone = document.querySelector("#contact_phone_number").value;
+  const message = document.querySelector("#contact_message").value;
+
+  console.log(first_name, last_name, email, phone, message);
+
+  fetch("https://sapdalimited.co.ke/php/", {
+    method: "POST",
+    body: JSON.stringify({
+      data: {
+        first_name,
+        last_name,
+        email,
+        phone,
+        message,
+        "contact-form": "true",
+      },
+    }),
+  })
+    .then((res) => {
+      console.log(res);
+
+      if (!res.ok) {
+        throw new Error("An error occured");
+      } else {
+        document.querySelector(".contact_form").reset();
+        document.querySelector(".response-sector").innerHTML = `
+        <div class="alert alert-success" role="alert">
+          Dear ${first_name} ${last_name},
+          Thank you for reaching out, a member of our organisation will get back to you shortly.
+        </div>
+        `;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      document.querySelector(".response-sector").innerHTML = `
+      <div class="alert alert-danger" role="alert">
+        An error occured, please try again later.
+      </div>
+      `;
+    });
+});
---
+++ php/index.php
@@ -0,0 +1,45 @@
+echo ' You are in the server';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['quote_form'])) {
+    // Get the data from the POST request
+    $name = $_POST['first_name'].' '.$_POST['last_name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    // Send email
+    $to = 'business@sapdalimited.co.ke';
+    $subject = 'A user wants to get a quote from website';
+    
+    $body = "First Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message";
+    
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo 'Email sent successfully';
+    } else {
+        echo 'Failed to send email';
+    }
+}
+
+
+//contact us processing
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['contact_form'])){
+    // Get the data from the POST request
+    $name = $_POST['first_name'].' '.$_POST['last_name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Send email
+    $to = 'contact-us@sapdalimited.co.ke';
+    $subject = 'New message from website';
+    $body = "Name: $name\nEmail: $email\nMessage: $message\nPhone: $phone";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo 'Email sent successfully';
+    } else {
+        echo 'Failed to send email';
+    }
+}
